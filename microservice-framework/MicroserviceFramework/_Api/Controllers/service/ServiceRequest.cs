a6628e320d10caf50a8a93a5bf49fde3e8e5f764
golinski-alexey <golinski.alexey@gmail.com>
2020-02-04 12:58:07 +0300

    из фреймворка убраны классы для работы с API запросами

--- microservice-framework/MicroserviceFramework/_Api/Controllers/service/ServiceRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RFI.MicroserviceFramework._Api.Controllers.service
-{
-    public class ServiceRequest : ApiRequest
-    {
-        public string Command { get; set; }
-    }
-}
--- microservice-framework/MicroserviceFramework/_Environment/SEnv.cs
+++ microservice-framework/MicroserviceFramework/_Environment/SEnv.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using RFI.MicroserviceFramework._Api.Controllers._health;
 using RFI.MicroserviceFramework._Loggers;
 
 namespace RFI.MicroserviceFramework._Environment
@@ -16,7 +15,7 @@ namespace RFI.MicroserviceFramework._Environment
             }
             catch(Exception ex)
             {
-                Healthes.Set("env", false);
+                //Healthes.Set("env", false);
                 ex.Log();
             }
         }
@@ -24,21 +23,7 @@ namespace RFI.MicroserviceFramework._Environment
 
         public static string Get(string name) => Environment.GetEnvironmentVariable(name);
 
-
         public static string EnvironmentName => Get("ASPNETCORE_ENVIRONMENT");
-
-        public static string Hostname => Get("HOSTNAME") ?? "Hostname not set";
-
-        public static string SsoKeyPublic => Get("SSO_KEY_PUBLIC");
-
-        public static string RfiKeyPrivate => Get("RFI_KEY_PRIVATE");
-
-        public static string RfiKeyPublic => Get("RFI_KEY_PUBLIC");
-
-        public static string OracleCS => Get("ORACLE_CS");
-
-
-        public static bool IsDevelopment => EnvironmentName == "Development";
 
         public static bool IsDebug { get; private set; }
     }
