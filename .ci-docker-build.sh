12efb7e39400198d986383de957eafe58100b67d
golinski-alexey <golinski.alexey@gmail.com>
2020-02-07 14:45:31 +0300

    no message

---
+++ .ci-docker-build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+JOB_STAGE=${1}
+PROJECT_NAME=${2}
+PROJECT_DIR=${3}
+PIPELINE_ID=${4}
+DOCKERFILE="/${PROJECT_DIR}/Dockerfile"
+
+
+if [[ $JOB_STAGE == "build" ]]; then
+
+# builder
+tee $DOCKERFILE > /dev/null << ---
+FROM dist.hosts.rfi:5000/dotnet/core/sdk:3.1-alpine
+COPY / /src
+RUN dotnet publish /src/${PROJECT_NAME} -c Release
+---
+docker pull dist.hosts.rfi:5000/${PROJECT_NAME}:builder || true
+docker build /${PROJECT_DIR} \
+  --tag dist.hosts.rfi:5000/${PROJECT_NAME}:builder \
+  --cache-from dist.hosts.rfi:5000/${PROJECT_NAME}:builder || exit 1
+docker push dist.hosts.rfi:5000/${PROJECT_NAME}:builder || exit 1
+
+
+elif [[ $JOB_STAGE == "deploy" ]]; then
+
+# deploy
+tee -a $DOCKERFILE > /dev/null << ---
+FROM dist.hosts.rfi:5000/dotnet/core/sdk:3.1-alpine
+COPY / /src
+RUN dotnet publish /src/${PROJECT_NAME} -c Release -o /app \
+RUN dotnet nuget push /src/${PROJECT_NAME}/bin/Release/*.nupkg --source http://dist.hosts.rfi:5555 --skip-duplicate \
+ && dotnet nuget push /src/${PROJECT_NAME}/bin/Release/*.snupkg --source http://dist.hosts.rfi:5555 --skip-duplicate
+---
+docker pull dist.hosts.rfi:5000/${PROJECT_NAME}:builder || true
+docker build /${PROJECT_DIR} --cache-from dist.hosts.rfi:5000/${PROJECT_NAME}:builder
+
+fi
+
+exit 0
