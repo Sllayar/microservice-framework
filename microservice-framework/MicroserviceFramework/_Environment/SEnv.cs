4fe782db3add4202e9806b9a6654bcd46c0f69ec
Голынский Алексей <agolyn@rfibank.ru>
2020-02-14 14:41:52 +0300

    Улучшен механизм хэлсчеков

--- microservice-framework/MicroserviceFramework/_Environment/SEnv.cs
+++ microservice-framework/MicroserviceFramework/_Environment/SEnv.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using RFI.MicroserviceFramework._Api.Controllers._health;
+using RFI.MicroserviceFramework._Api;
 using RFI.MicroserviceFramework._Loggers;
 
 namespace RFI.MicroserviceFramework._Environment
@@ -16,7 +16,7 @@ namespace RFI.MicroserviceFramework._Environment
             }
             catch(Exception ex)
             {
-                Healthes.Set("env", false);
+                ApiHealth.Set("env", false);
                 ex.Log();
             }
         }
